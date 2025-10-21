38b1df3b7dae5d419ff5feb30dd199a4b9be31c7
radit <rdithardiansyah@gmail.com>
2025-10-21 16:26:53 +0700

    implement things i learned today

--- makef.go
+++ makef.go
@@ -2,15 +2,45 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	pam := make(map[int]bool)
-	pam[0] = true
-	v, ok := pam[0]
-	if ok {
-		fmt.Println("Value:", v)
+
+	var qa [5]int = [5]int{10, 20, 30, 40, 50}
+
+	qs := qa[:]
+
+	var nq = make([]int, 0, 10)
+
+	nq = append(nq, qs...)
+
+	var fixedArray [5]int
+	copy(fixedArray[:], nq)
+
+	inventory := make(map[string]int)
+
+	itemNames := []string{"john", "paul", "george", "ringo", "billy"}
+	for i, beatles := range itemNames {
+		lowerName := strings.ToLower(beatles)
+		inventory[lowerName] = fixedArray[i]
 	}
-	delete(pam, 0)
+
+	checkItems := []string{"john", "paul", "ringo"}
+	for _, item := range checkItems {
+		qty, ok := inventory[item]
+		if ok {
+			fmt.Printf("%s: %d units\n", item, qty)
+		} else {
+			fmt.Printf("%s: not found in inventory\n", item)
+		}
+	}
+
+	fmt.Println("Capacity before append:", cap(nq))
+	for i := 0; i < 10; i++ {
+		nq = append(nq, 1)
+
+	}
+	fmt.Println("Capacity after append:", cap(nq))
 
 }
