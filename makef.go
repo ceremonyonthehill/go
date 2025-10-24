af2f3364e35b29ac74a06686aea9b02666bc5c42
radit <rdithardiansyah@gmail.com>
2025-10-24 17:06:36 +0700

    finish if/elseif/switchcase

--- makef.go
+++ makef.go
@@ -1,40 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 )
 
-type Person struct {
-	Name string `json:"n,omitempty"`
-	Age  int    `json:"a,omitempty"`
-}
-type E struct {
-	Person
-	EmployeeId string
-}
-
 func main() {
-	p := E{Person: Person{Name: "Wanwan", Age: 10},
-		EmployeeId: "12345E"}
-	fmt.Println(p.Name)
-	fmt.Println(p.EmployeeId)
-
-	jsonData, err := json.Marshal(p)
-	if err != nil {
-		fmt.Println("Error marshaling JSON:", err)
-		return
+	one := 1
+	two := 2
+	three := 3
+	if one+two == three {
+		fmt.Println("thats  ////")
+	} else {
+		fmt.Println("thats not right")
 	}
-
-	fmt.Println(string(jsonData))
-
-	var p2 Person
-	jsonStr := `{"n":"Marley","a":25}`
-	err = json.Unmarshal([]byte(jsonStr), &p2)
-	if err != nil {
-		fmt.Println("Error unmarshaling JSON:", err)
-		return
-	}
-	fmt.Printf("Unmarshaled struct %+v\n", p2)
 
 }
